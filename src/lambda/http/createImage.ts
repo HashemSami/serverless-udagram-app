779722237dce1a8c41cea43d7938cc5581a79b51
Hashem Sami <hashem.sf@gmail.com>
2020-12-18 19:55:13 +0300

    added s3 bucket image download

--- src/lambda/http/createImage.ts
+++ src/lambda/http/createImage.ts
@@ -1,14 +1,25 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 import "source-map-support/register";
 import * as AWS from "aws-sdk";
 import * as uuid from "uuid";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3({
+  signatureVersion: "v4",
+});
 
 const groupsTable = process.env.GROUPS_TABLE;
 const imagesTable = process.env.IMAGES_TABLE;
+const bucketName = process.env.IMAGES_S3_BUCKET;
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION);
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   console.log("Caller event", event);
   const groupId = event.pathParameters.groupId;
   const validGroupId = await groupExists(groupId);
@@ -17,25 +28,28 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     return {
       statusCode: 404,
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
       },
       body: JSON.stringify({
-        error: "Group does not exist"
-      })
+        error: "Group does not exist",
+      }),
     };
   }
 
   const imageId = uuid.v4();
   const newItem = await createImage(groupId, imageId, event);
 
+  const url = getUploadUrl(imageId);
+
   return {
     statusCode: 201,
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      newItem: newItem
-    })
+      newItem: newItem,
+      uploadUrl: url,
+    }),
   };
 };
 
@@ -44,8 +58,8 @@ async function groupExists(groupId: string) {
     .get({
       TableName: groupsTable,
       Key: {
-        id: groupId
-      }
+        id: groupId,
+      },
     })
     .promise();
 
@@ -61,16 +75,25 @@ async function createImage(groupId: string, imageId: string, event: any) {
     groupId,
     timestamp,
     imageId,
-    ...newImage
+    ...newImage,
+    imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}`,
   };
   console.log("Storing new item: ", newItem);
 
   await docClient
     .put({
       TableName: imagesTable,
-      Item: newItem
+      Item: newItem,
     })
     .promise();
 
   return newItem;
+}
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl("putObject", {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration,
+  });
 }
